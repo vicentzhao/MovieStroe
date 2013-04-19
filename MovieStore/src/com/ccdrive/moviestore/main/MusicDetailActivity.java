6b531340abf653180124c6b860f1fffde3076f7d
sl <sl@Jay-PC>
2013-04-19 20:05:51 +0800

    一些小的改动

---
+++ MovieStore/src/com/ccdrive/moviestore/main/MusicDetailActivity.java
@@ -0,0 +1,16 @@
+package com.ccdrive.moviestore.main;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MusicDetailActivity extends Activity {
+
+	  @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+	}
+	  
+}
